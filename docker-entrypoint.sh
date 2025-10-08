0ea53d80618b7ba0aea155f89d90d3dbf07a86b6
David Ichim <ichim.david@gmail.com>
2025-10-08 15:24:58 +0300

    Revert "Update docker-entrypoint.sh for initialization and tests"
    This reverts commit f4b9d8fd4e2849af4925f3ec49a1e24fe45c75b7.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
-gosu plone python /docker-initialize.py
+
+#gosu plone python /app/docker-initialize.py
+
+
 
 if [ -z "$GIT_URL" ]; then
   GIT_URL="https://github.com"
@@ -15,11 +18,16 @@ if [ -z "$GIT_USER" ]; then
   GIT_USER="eea"
 fi
 
+cd /app/
+
+
 LOCATION=$(pwd)
 if [ ! -z "$DEVELOP" ]; then
   for dev in $DEVELOP; do
+    dev=$(echo "$dev" | sed 's#/app/##')	  
     if [ ! -d $dev ]; then
       GIT=`echo $GIT_URL/$dev | sed "s|src|$GIT_USER|g"`
+      echo $dev
       echo "Cloning from $GIT"
       gosu plone git clone -v $GIT $dev
       cd $dev
@@ -32,35 +40,20 @@ if [ ! -z "$DEVELOP" ]; then
     fi
   done
 
-  if [ -e "custom.cfg" ]; then
-    echo -e "[versions]" >> custom.cfg
-    for addon in $ADDONS; do
-      addon="$(echo $addon | sed 's|\[.*\]||g')"
-      echo -e "$addon =" >> custom.cfg
+fi
+
+if [[ "$@" == "start" ]] || [ $# -eq 0 ]; then
+  if [ -n "$DEVELOP" ]; then
+    for dev in $DEVELOP; do
+      if [ -d $dev ]; then
+        dev=$(echo "$dev" | sed 's#/app/##')
+        echo "Running tests for $dev"
+        args="bin/zope-testrunner --auto-color --auto-progress --test-path /app/$dev"
+	/app/plone-entrypoint.sh bin/zope-testrunner --auto-color --auto-progress --coverage /app/coverage --test-path /app/$dev
+      fi
     done
   fi
+else
+   exec /app/plone-entrypoint.sh "$@"
 fi
 
-if [[ "$1" == "zptlint"* ]]; then
-  echo "Running bin/zptlint src/"
-  find src -regex ".*\.[c|z]*pt" -print0 | xargs -0 -r bin/zptlint
-  exit
-fi
-
-if [ -e "custom.cfg" ]; then
-  buildout -c custom.cfg
-  chown -R plone:plone /plone
-fi
-
-if [[ "$1" == "coverage"* ]]; then
-    cd src/$GIT_NAME
-    ../../bin/coverage run ../../bin/xmltestreport --test-path $(pwd) -v -vv -s $GIT_NAME
-    ../../bin/report xml --include=*$GIT_NAME*
-    exit 0
-fi
-
-if [[ "$1" == "-"* ]]; then
-  exec bin/test "$@"
-fi
-
-exec /plone-entrypoint.sh "$@"
